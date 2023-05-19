08c84fa61a061b4cc570f79adfce69f6f990f5cd
Adipp <emangadayanglain@gmail.com>
2023-05-19 07:31:27 +0700

    fix : [A] UI Error FIxed

--- app/src/main/java/com/example/flixgo/OnBoarding.java
+++ app/src/main/java/com/example/flixgo/OnBoarding.java
@@ -1,14 +1,82 @@
 package com.example.flixgo;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.viewpager.widget.ViewPager;
 
+import android.location.GnssAntennaInfo;
 import android.os.Bundle;
+import android.text.Html;
+import android.view.View;
+import android.widget.Button;
+import android.widget.LinearLayout;
+import android.widget.TextView;
 
 public class OnBoarding extends AppCompatActivity {
+
+    ViewPager viewPager;
+    Button btnNext;
+    LinearLayout dotLayout;
+    ViewPagerAdapter viewPagerAdapter;
+    TextView[] dots;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_on_boarding);
+
+        btnNext = findViewById(R.id.btnnext);
+
+        btnNext.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
+
+        viewPager = (ViewPager) findViewById(R.id.viewpager);
+        dotLayout = (LinearLayout) findViewById(R.id.indicator_layout);
+
+        viewPagerAdapter = new ViewPagerAdapter(this);
+        viewPager.setAdapter(viewPagerAdapter);
+
     }
+
+    public void SetUpIndicator (int position) {
+
+        dots = new TextView[4];
+        dotLayout.removeAllViews();
+
+        for (int i = 0; i < dots.length; i++) {
+
+            dots[i] = new TextView(this);
+            dots[i].setText(Html.fromHtml("&#8226"));
+            dots[i].setTextSize(35);
+            dots[i].setTextColor(getResources().getColor(R.color.inactive, getApplicationContext().getTheme()));
+            dotLayout.addView(dots[i]);
+        }
+
+        dots[position].setTextColor(getResources().getColor(R.color.white, getApplicationContext().getTheme()));
+    }
+
+    ViewPager.OnPageChangeListener viewListener = new ViewPager.OnPageChangeListener() {
+        @Override
+        public void onPageScrolled(int position, float positionOffset, int positionOffsetPixels) {
+
+        }
+
+        @Override
+        public void onPageSelected(int position) {
+
+            SetUpIndicator(position);
+
+            if (position > 0)
+
+        }
+
+        @Override
+        public void onPageScrollStateChanged(int state) {
+
+        }
+    };
 }
---
+++ app/src/main/java/com/example/flixgo/ViewPagerAdapter.java
@@ -0,0 +1,61 @@
+package com.example.flixgo;
+
+import android.content.Context;
+import android.media.Image;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
+
+import androidx.annotation.NonNull;
+import androidx.viewpager.widget.PagerAdapter;
+import androidx.viewpager.widget.ViewPager;
+
+import kotlin.io.LineReader;
+
+public class ViewPagerAdapter extends PagerAdapter {
+    Context context;
+
+    int images[] = {
+            R.drawable.onBoardingImage_1,
+            R.drawable.onBoardingImage_2,
+            R.drawable.onBoardingImage_3
+    };
+
+    public ViewPagerAdapter(Context context) {
+        this.context = context;
+
+    }
+
+    @Override
+    public int getCount() {
+        return images.length;
+    }
+
+    @Override
+    public boolean isViewFromObject(@NonNull View view, @NonNull Object object) {
+        return view == (LinearLayout) object;
+
+    }
+
+    @NonNull
+    @Override
+    public Object instantiateItem(@NonNull ViewGroup container, int position) {
+
+        LayoutInflater layoutInflater = (LayoutInflater) context.getSystemService(context.LAYOUT_INFLATER_SERVICE);
+        View view = layoutInflater.inflate(R.layout.slider_layout, container, false);
+
+        ImageView slideimage = (ImageView) view.findViewById(R.id.ivslider);
+        slideimage.setImageResource(images[position]);
+
+        container.addView(view);
+        return view;
+    }
+
+    @Override
+    public void destroyItem(@NonNull ViewGroup container, int position, @NonNull Object object) {
+        container.removeView((LinearLayout)object);
+
+    }
+}
